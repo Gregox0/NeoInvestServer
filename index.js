dcd18a0b08323ae59eeb3edeb79566a3c546d2ca
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2025-02-25 07:52:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 
 const app = express()
 const FINNHUB_API_KEY = 'cuuboshr01qlidi319tgcuuboshr01qlidi319u0'
-  
+
 app.use(cors())
 
 app.get('/stock/:symbol', async (req, res) => {
@@ -14,29 +14,33 @@ app.get('/stock/:symbol', async (req, res) => {
     const companyResponse = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${FINNHUB_API_KEY}`)
     const companyData = await companyResponse.json()
 
-    const quoteResponse = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${FINNHUB_API_KEY}`);
+    const quoteResponse = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${FINNHUB_API_KEY}`)
     const quoteData = await quoteResponse.json()
 
     const currentPrice = quoteData.c
     const previousClose = quoteData.pc
 
-    let appreciation = ((currentPrice - previousClose) / previousClose) * 100
+    let appreciation = ((currentPrice - previousClose) / previousClose) * 100;
     appreciation = appreciation.toFixed(2)
 
     if (appreciation === 'NaN') appreciation = '0.00'
     if (appreciation > 100) appreciation = '100+'
 
-    res.json({
+    const stockData ={
       symbol: symbol,
       companyName: companyData.name || "Nome não encontrado",
       currentPrice: currentPrice.toFixed(2),
       appreciation: appreciation,
-    })
+    }
+
+    res.json(stockData)
+
   } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar dados da ação" });
+    console.error("Erro ao buscar dados:", error)
+    res.status(500).json({ error: "Erro ao buscar dados da ação" })
   }
-});
+})
 
 app.listen(5555, () => {
-  console.log('Server rodando na porta 5555');
-});
+  console.log('Server rodando na porta 5555')
+})
